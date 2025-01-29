4aa0ceda5a33002d8ee4669a4dbc2180ec441bd0
teyper <teyjhanabartonneal@spelman.edu>
2025-01-29 10:18:59 -0500

    final touches

--- Assets/ASSETS.cs
+++ Assets/ASSETS.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class ASSETS : MonoBehaviour
 {
 
-    //Textures from texturebox for floor, 
-    //Textures from ambientcg for frames, shelves, bases and wires
+    //Textures from texturebox for floor cieling , https://texturebox.com/category/wood-textures/flooring-textures
+    //Textures from ambientcg for frames, shelves, bases and wires,
     //Textures for rug and couch and bench cushio from Unity.
     //Photos: https://www.pinterest.com/pin/7318418140189604/ (akatsuki)
     // https://www.pinterest.com/pin/90001692548353992/ (wunna)
+    //https://www.pinterest.com/pin/837880705701800974/ (angela davis)
     //https://www.pinterest.com/pin/101260691617093056/ (kill bill)
+    //https://www.pinterest.com/pin/4785143350569289/ (chun li)
     // bg music: https://youtu.be/xv_lFCQGjvQ?si=GVvwf2aGOrXehlaa
-   //
-    //
-
-    //
+    //plants: https://www.turbosquid.com/AssetManager/Index.cfm?stgAction=getFiles&subAction=Download&intID=1372214&intType=3&csrf=3B776BBAA4C8C079448890A1C6BC92267A3303C1&showDownload=1&s=1
+    //leaves: https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.plantsforallseasons.co.uk%2Fblogs%2Fbonsai-care%2Fwhy-is-my-bonsai-dropping-leaves-and-how-to-solve-it&psig=AOvVaw2W4PYM9SytaYzTPeqXjKsL&ust=1738248548036000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCOCFscuWm4sDFQAAAAAdAAAAABAE
+    // tv screen: https://www.pinterest.com/pin/424464333649866688/ (lyrics)
+    //books: (bible) https://www.google.com/url?sa=i&url=https%3A%2F%2Ftanbooks.com%2Fproducts%2Fbooks%2Fdouay-rheims-bible-large-print-edition%2F&psig=AOvVaw0wnuBvQT4FeSNVkf1HNYfw&ust=1738248809704000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNDH_siXm4sDFQAAAAAdAAAAABAE
+    // (marcus garvey) https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.com%2FPhilosophy-Opinions-Marcus-Africans-Library%2Fdp%2F0912469242&psig=AOvVaw0B3POphsz86l9E-yq4f1RQ&ust=1738248837384000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCOjI1NWXm4sDFQAAAAAdAAAAABAE
     // Start is called before the first frame update
     void Start()
     {
